5a436b3a75dc228c3cf0b0b645fa3608ec7822bc
JaninaHeikkala <janina.heikkala@hotmail.com>
2024-09-09 19:27:30 +0300

    Up until 4.7 done

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -15,8 +15,44 @@ const favoriteBlog = (blogs) => {
   }, blogs[0]);
 }
 
+const mostBlogs = (blogs) => {
+  const authors = []
+  blogs.forEach(blog => {
+    const author = authors.find(a => a.author === blog.author); // try to find current author in authors
+    if (!author) { // if the author can not be found from the authors array
+      authors.push({ author: blog.author, blogs: 1 })
+    }
+    else {
+      author.blogs += 1 // if found increment their amount of blogs
+    }
+  })
+  // finding the author with most blogs
+  return authors.reduce((max, author) => {
+    return author.blogs > max.blogs ? author : max;
+  });
+}
+
+const mostLikes = (blogs) => {
+  const authors = []
+  blogs.forEach(blog => {
+    const author = authors.find(a => a.author === blog.author);
+    if (!author) { // if the author can not be found from the authors array
+      authors.push({ author: blog.author, likes: blog.likes }) // add it
+    }
+    else {
+      author.likes += blog.likes // if found increment their total amount of likes
+    }
+  })
+  // finding the author with most likes
+  return authors.reduce((max, author) => {
+    return author.likes > max.likes ? author : max;
+  });
+}
+
 module.exports = {
   dummy,
   totalLikes,
-  favoriteBlog
+  favoriteBlog,
+  mostBlogs,
+  mostLikes,
 }
