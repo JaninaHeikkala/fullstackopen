b5878df39a3f607e7aa11501750cfe384c1ef9dd
JaninaHeikkala <janina.heikkala@hotmail.com>
2024-12-12 20:36:05 +0200

    5.14

--- part5/bloglist-frontend/src/components/Blog.test.jsx
+++ part5/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,9 +1,8 @@
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import Blog from './Blog.jsx'
 
 test('renders content', async () => {
-  const user = userEvent.setup()
 
   const blog = {
     title: 'testTitle1',
@@ -24,5 +23,30 @@ test('renders content', async () => {
 
   expect(container.querySelector('.blog-url')).toBeNull()
   expect(container.querySelector('.blog-likes')).toBeNull()
+
+})
+
+test('details click', async () => {
+  const user = userEvent.setup()
+
+  const blog = {
+    title: 'testTitle1',
+    author: 'testAuthor1',
+    url: 'testUrl1',
+    likes: 0,
+    user: 'testUser1',
+  }
+
+  const { container } = render(<Blog blog={blog}/>)
+
+  const button = screen.getByText('show details')
+  await user.click(button)
+
+  const detailsElement = container.querySelector('.blog-details')
+  expect(detailsElement).toHaveTextContent('testUrl1')
+  expect(detailsElement).toHaveTextContent('likes 0')
+
+  expect(container.querySelector('.blog-url')).not.toBeNull()
+  expect(container.querySelector('.blog-likes')).not.toBeNull()
 
 })
